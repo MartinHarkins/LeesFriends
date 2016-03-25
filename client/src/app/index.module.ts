b1f8a5a4779204696ae0ee07278954a0c3124731
Martin Harkins <mharkins@grio.com>
2016-03-25 14:12:42 -0700

    Fix package son

--- client/src/app/index.module.ts
+++ client/src/app/index.module.ts
@@ -16,33 +16,33 @@ declare var malarkey: any;
 declare var moment: moment.MomentStatic;
 
 module leesFriends {
-  'use strict';
+    'use strict';
 
-  angular.module('leesFriends.components', [])
-    .component('home', new HomeComponent())
-    .component('carousel', new CarouselComponent())
-    .component('awesomeThings', new ATComponent())
-    .directive('header', () => new HeaderDirective())
-    .directive('services', () => new ServicesDirective());
+    angular.module('leesFriends.components', [])
+        .component('home', new HomeComponent())
+        .component('carousel', new CarouselComponent())
+        .component('awesomeThings', new ATComponent())
+        .directive('header', () => new HeaderDirective())
+        .directive('services', () => new ServicesDirective());
 
-  angular.module('leesFriends', [
-      'ngAnimate',
-      'ngCookies',
-      'ngTouch',
-      'restangular',
-      'ui.router',
-      'ui.bootstrap',
-      'toastr',
-      'leesFriends.components'
+    angular.module('leesFriends', [
+        'ngAnimate',
+        'ngCookies',
+        'ngTouch',
+        'restangular',
+        'ui.router',
+        'ui.bootstrap',
+        'toastr',
+        'leesFriends.components'
     ])
-    .constant('malarkey', malarkey)
-    .constant('moment', moment)
-    .config(config)
-    .config(routerConfig)
-    .run(runBlock)
-    .service('githubContributor', GithubContributor)
-    .service('webDevTec', WebDevTecService)
-    .controller('MainController', MainController)
-    .directive('acmeNavbar', acmeNavbar)
-    .directive('acmeMalarkey', acmeMalarkey);
+        .constant('malarkey', malarkey)
+        .constant('moment', moment)
+        .config(config)
+        .config(routerConfig)
+        .run(runBlock)
+        .service('githubContributor', GithubContributor)
+        .service('webDevTec', WebDevTecService)
+        .controller('MainController', MainController)
+        .directive('acmeNavbar', acmeNavbar)
+        .directive('acmeMalarkey', acmeMalarkey);
 }
