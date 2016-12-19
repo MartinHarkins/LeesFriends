4844380c48991ef7d05ec1fef0b8402bb6dd4492
Martin Harkins <martin.a.harkins@gmail.com>
2016-12-19 19:00:19 +0700

    Add documentation for ResponseWrapper

--- server/src/core/response-wrapper.ts
+++ server/src/core/response-wrapper.ts
@@ -1,5 +1,16 @@
 import {Error} from "../common/error";
 
+/**
+ * A wrapper for responses returned in an observable
+ *
+ * Errors thrown in a node callback aren't caught by the observable.
+ * This means the app crashes when there are errors, while it should not.
+ *
+ * We're using a wrapper to return a package containing either the response or the error.
+ *
+ * This also follows RxJava best practices where only critical errors should result in an error for the subscriber.
+ *
+ */
 export class ResponseWrapper<T> {
     public response: T;
     public error: Error;
