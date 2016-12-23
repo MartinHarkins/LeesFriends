48c876109e1c8716c172307d985876403b03cb5b
Martin Harkins <martin.a.harkins@gmail.com>
2016-12-23 16:04:39 +0700

    Update font files

--- client/src/app/components/header/header.ts
+++ client/src/app/components/header/header.ts
@@ -6,25 +6,25 @@ import {Component} from "@angular/core";
       <div class="container-fluid header margin-bottom-lg">
         <div class="row">
           <ul class="menu">
-            <li class="margin-top-lg"><a [routerLink]=" './services' "><h2>Services</h2></a></li>
-            <li class="margin-top-sm"><a [routerLink]=" './our-mission' "><h2>Our Mission</h2></a></li>
-            <li class="margin-top-md"><a [routerLink]=" './history' "><h2>History</h2></a></li>
+            <li class="margin-top-lg"><a [routerLink]=" './services' " routerLinkActive="active-link"><h2>Services</h2></a></li>
+            <li class="margin-top-sm"><a [routerLink]=" './our-mission' " routerLinkActive="active-link"><h2>Our Mission</h2></a></li>
+            <li class="margin-top-md"><a [routerLink]=" './history' " routerLinkActive="active-link"><h2>History</h2></a></li>
             <li>
-              <a [routerLink]=" './home' ">
+              <a [routerLink]=" './home' " routerLinkActive="active-link">
                 <h1 class="title">
                   Lee's Friends
                 </h1>
               </a>
             </li>
-            <li class="margin-top-md"><a [routerLink]=" './events' "><h2>Events</h2></a></li>
-            <li class="margin-top-sm"><a [routerLink]=" './awards' "><h2>Awards</h2></a></li>
-            <li class="margin-top-md"><a [routerLink]=" './about-us' "><h2>About Us</h2></a></li>
-            <li class="margin-top-lg"><a [routerLink]=" './faq' "><h2>FAQ</h2></a></li>
+            <li class="margin-top-md"><a [routerLink]=" './events' " routerLinkActive="active-link"><h2>Events</h2></a></li>
+            <li class="margin-top-sm"><a [routerLink]=" './awards' " routerLinkActive="active-link"><h2>Awards</h2></a></li>
+            <li class="margin-top-md"><a [routerLink]=" './about-us' " routerLinkActive="active-link"><h2>About Us</h2></a></li>
+            <li class="margin-top-lg"><a [routerLink]=" './faq' " routerLinkActive="active-link"><h2>FAQ</h2></a></li>
           </ul>
         </div>  
         <div class="row">
             <div class="pull-right more">
-                <a [routerLink]=" './contact-us' "><h5>Contact Us</h5></a>
+                <a [routerLink]=" './contact-us' " routerLinkActive="active-link"><h5>Contact Us</h5></a>
             </div>
         </div>
       </div>
--- client/src/app/components/no-content/no-content.component.ts
+++ client/src/app/components/no-content/no-content.component.ts
@@ -4,7 +4,7 @@ import {Component} from "@angular/core";
   selector: 'no-content',
   template: `
     <div>
-      <h1>404: page missing</h1>
+      <h1>Sorry, page missing! (404)</h1>
     </div>
   `
 })
