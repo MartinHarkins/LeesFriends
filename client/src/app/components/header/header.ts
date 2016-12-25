15ffc772bc73fe9521a420470f67794b52e4f5f2
Martin Harkins <martin.a.harkins@gmail.com>
2016-12-25 11:01:43 +0700

    Responsive Header

--- client/src/app/components/header/header.ts
+++ client/src/app/components/header/header.ts
@@ -4,24 +4,26 @@ import {Component} from "@angular/core";
   selector: 'header',
   template: `
       <div class="container-fluid header">
-        <div class="row">
-          <ul class="menu">
-            <li class="margin-top-lg"><a [routerLink]=" './services' " routerLinkActive="active-link"><h2>Services</h2></a></li>
-            <li class="margin-top-sm"><a [routerLink]=" './our-mission' " routerLinkActive="active-link"><h2>Our Mission</h2></a></li>
-            <li class="margin-top-md"><a [routerLink]=" './history' " routerLinkActive="active-link"><h2>History</h2></a></li>
-            <li>
+        <div class="row menu">
+          <div class="menu-group menu-group-left">
+            <div class="menu-item margin-top-lg"><a [routerLink]=" './services' " routerLinkActive="active-link"><h2>Services</h2></a></div>
+            <div class="menu-item margin-top-sm"><a [routerLink]=" './our-mission' " routerLinkActive="active-link"><h2>Our Mission</h2></a></div>
+            <div class="menu-item margin-top-md"><a [routerLink]=" './history' " routerLinkActive="active-link"><h2>History</h2></a></div>
+          </div>
+          <div class="menu-group menu-group-center" >
               <a [routerLink]=" './home' " routerLinkActive="active-link">
-                <h1 class="title">
-                  Lee's Friends
-                </h1>
-              </a>
-              <h3 class="subtitle">Helping People Live With Cancer</h3>
-            </li>
-            <li class="margin-top-md"><a [routerLink]=" './events' " routerLinkActive="active-link"><h2>Events</h2></a></li>
-            <li class="margin-top-sm"><a [routerLink]=" './awards' " routerLinkActive="active-link"><h2>Awards</h2></a></li>
-            <li class="margin-top-md"><a [routerLink]=" './about-us' " routerLinkActive="active-link"><h2>About Us</h2></a></li>
-            <li class="margin-top-lg"><a [routerLink]=" './faq' " routerLinkActive="active-link"><h2>FAQ</h2></a></li>
-          </ul>
+              <h1 class="title">
+                Lee's Friends
+              </h1>
+            </a>
+            <h3 class="subtitle">Helping People Live With Cancer</h3>
+          </div>
+          <div class="menu-group menu-group-right">
+            <div class="menu-item margin-top-md"><a [routerLink]=" './events' " routerLinkActive="active-link"><h2>Events</h2></a></div>
+            <div class="menu-item margin-top-sm"><a [routerLink]=" './awards' " routerLinkActive="active-link"><h2>Awards</h2></a></div>
+            <div class="menu-item margin-top-md"><a [routerLink]=" './about-us' " routerLinkActive="active-link"><h2>About Us</h2></a></div>
+            <div class="menu-item margin-top-lg"><a [routerLink]=" './faq' " routerLinkActive="active-link"><h2>FAQ</h2></a></div>
+          </div>
         </div>  
         <div class="row">
             <div class="pull-right more">
